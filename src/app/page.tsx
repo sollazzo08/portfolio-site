63885a277bf8eacdf8644d153ec830f2294726ac
Sollazzo08 <msollazzo08@gmail.com>
2025-04-27 16:51:43 -0400

    update image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
       <div className="flex flex-col space-y-7 bg-[#191919]">
         <section className="flex">
           <Image
-          src="/public/IMG_1915.png" // Replace with actual path
+          src="/IMG_1915.png" // Replace with actual path
           alt="Michael Sollazzo"
           width={80} // Adjust as needed
           height={80} // Adjust as needed

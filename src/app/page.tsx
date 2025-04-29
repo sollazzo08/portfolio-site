7b2df21b80f24aab6e5bd603022ac5d58f3b67d2
Sollazzo08 <msollazzo08@gmail.com>
2025-04-28 21:38:11 -0400

    added ease in animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,19 @@
+'use client'
+
 import ProjectsList from "@/components/ProjectsList";
 import Link from "next/link";
+
+import {motion} from 'framer-motion';
 
 export default function Home() {
   return (
     <div className="flex flex-col text-gray-400 font-tektu text-md">
-      <div className="flex flex-col space-y-7 bg-[#191919]">
+      <motion.div
+          initial={{ opacity: 0, scale: 0.95 }} // Start hidden and a little lower
+          animate={{ opacity: 1, scale: 1 }}  // Animate to fully visible and back to normal position
+          transition={{ duration: 1.5, ease: 'easeOut' }} // Timing
+          className="relative flex flex-col space-y-7 bg-[#191919]"
+      >
         <section className="flex">
           <img
           src="/mike&sawyer3.jpg"
@@ -24,7 +33,8 @@ export default function Home() {
         </section>
           <p>I am Technical Consultant & Developer who leads all Salsify PIM implementations at Huge | Hero Digital.
           </p>
-      </div>
+      </motion.div>
+
       <Link href="/about" className="mt-3 underline text-[#ebeaea]">Learn More</Link>
       <div className="flex flex-col space-y-7 mt-10 bg-[#191919]">
       <h1 className="text-lg underline text-[#ebeaea]">Recent Projects</h1>
